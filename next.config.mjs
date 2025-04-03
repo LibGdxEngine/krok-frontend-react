18e800f6521d4cb572605006804c754f67236314
LibGdxEngine <libgdxengine@gmail.com>
2025-04-03 20:21:54 +0200

    shop

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 import nextI18NextConfig from './next-i18next.config.mjs';
 
 const nextConfig = {
+  images: {
+    domains: ['krokplus.com'],
+  },
   reactStrictMode: true,
   swcMinify: true,  // Use SWC for minification instead of Terser
   ...nextI18NextConfig,  // Spread the i18n configuration into the Next.js config
