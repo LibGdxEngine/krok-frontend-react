103c0cfb53584f553397bcfd724af3e8896b8b7e
LibGdxEngine <libgdxengine@gmail.com>
2025-11-25 21:30:59 +0200

    fix

--- src/components/axiosInstance.js
+++ src/components/axiosInstance.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    // baseURL: 'https://krokplus.com/api',
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://krokplus.com/api',
+    // baseURL: 'http://localhost:8000/api',
     headers: {
         'Content-Type': 'application/json',
     },
